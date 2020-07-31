b72bdbb5e49c2529220a8acf47577569c6edc6a3
No360201 <57511687+No360201@users.noreply.github.com>
2020-07-31 17:29:03 +0800

    Update net.py

--- subway_detector/class_2/net.py
+++ subway_detector/class_2/net.py
@@ -1,7 +1,7 @@
 import torch.nn as nn
 import math
 import torch.utils.model_zoo as model_zoo
-from config import Config
+from subway_detector.config import Config
 
 __all__ = ['ResNet', 'resnet34', 'resnet50', 'resnet101', 'resnet152']
 
